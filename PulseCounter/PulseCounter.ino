d29808959ca92ecd832ac923cfbbab8eebc46dac
ErikOwsiak <erik@owsiak.name>
2019-09-21 12:04:54 +0200

    program init

---
+++ PulseCounter/PulseCounter.ino
@@ -0,0 +1,15 @@
+
+/*
+   simple program to count pulses
+*/
+
+void setup() {
+   // put your setup code here, to run once:
+
+}
+
+
+void loop() {
+   // put your main code here, to run repeatedly:
+
+}
